e3096f0dc25daf08abbf6b3472b99c6612a30317
crtr <chro64@mailfence.com>
2025-07-25 10:23:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -84,4 +84,5 @@ def voice():
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
